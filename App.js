1da400955e10801c08bc09fbf7e35396c7695d27
Ada Meceffeuk <46904388+averroes96@users.noreply.github.com>
2020-06-26 21:59:25 +0200

    Learned useState, StyleSheet, ScrollView and FlatList

--- App.js
+++ App.js
@@ -1,42 +1,81 @@
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
+
+  const [goal, setGoal] = useState("");
+  const [goals, setGoals] = useState([]);
+
+  const inputHandler = (value) => {
+    setGoal(value);
+  }
+
+  const addBtnHandler = () => {
+    setGoals(currentGoals => [
+      ...currentGoals, 
+      {
+        key: Math.random().toString(), 
+        value: goal
+      }
+    ]);
+  }
+
   return (
-    <View 
-      style = {{
-        flexDirection:"row", 
-        padding:20,
-        width:'100%',
-        height: 300,
-        justifyContent:"space-between",
-        alignItems:'stretch'
-         }}>
-      <View 
-        style = {{
-          backgroundColor:"#eee",
-          justifyContent:"center", 
-          alignItems:"center",
-          flex:3
-          }}>
-      <TextInput 
-        placeholder="Your goal?" 
-        style={{borderBottomWidth:1, borderBottomColor:"#aaa", padding:10, width:'80%'}} 
-        />
+    <View style={{flex:1}}>
+      <View style = {styles.screen}>
+        <View 
+          style = {styles.inputContainer}>
+        <TextInput 
+          placeholder="Your goal?" 
+          style={styles.input} 
+          onChangeText={inputHandler}
+          value={goal}
+          />  
+        </View>
+        <View 
+          style = {styles.buttonContainer}>
+          <Button title="ADD GOAL" onPress={addBtnHandler} />
+        </View>
       </View>
-      <View 
-        style = {{
-          backgroundColor:"#ddd",
-          justifyContent:"center", 
-          alignItems:"center",
-          flex:2          
-          }}>
-         <Button title="ADD" />
-      </View>
+      <FlatList
+        data={goals} 
+        renderItem={goal => 
+          <View>
+          <Text style={styles.goals}>{goal.item.value}</Text>
+          </View>        
+      }>
+      </FlatList>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-
+  screen:{
+    flexDirection:"row", 
+    padding:20,
+    width:'100%',
+    height: 150,
+    justifyContent:"space-between",
+    alignItems:'stretch'    
+  },
+  inputContainer:{
+    backgroundColor:"#eee",
+    justifyContent:"center", 
+    alignItems:"center",
+    flex:3
+  },
+  input:{
+    borderBottomWidth:1, 
+    borderBottomColor:"#aaa", 
+    padding:10, width:'80%'
+  },
+  buttonContainer:{
+    backgroundColor:"#ddd",
+    justifyContent:"center", 
+    alignItems:"center",
+    flex:2   
+  },
+  goals:{
+    marginVertical:10
+  }
 });
